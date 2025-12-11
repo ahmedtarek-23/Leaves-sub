ffa901cbe863fea93c9c60baf7d35aed248cddda
Rana Abouraia <16abourr@gmail.com>
2025-12-11 23:28:45 +0200

    added modules again to fix missing files

--- backend/src/auth/permissions.constant.ts
+++ backend/src/auth/permissions.constant.ts
@@ -68,6 +68,9 @@ export const ROLE_PERMISSIONS: Record<SystemRole, Permission[]> = {
     Permission.VIEW_ORG_STRUCTURE,
   ],
 
+  [SystemRole.PAYROLL_MANAGER]: [],   // ← insert this line
+
+
   [SystemRole.FINANCE_STAFF]: [
     Permission.APPROVE_PAYROLL,
   ],
--- backend/src/employee-profile/employee-profile.module.ts
+++ backend/src/employee-profile/employee-profile.module.ts
@@ -19,9 +19,11 @@ import {
   EmployeeQualification,
   EmployeeQualificationSchema,
 } from './models/qualification.schema';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     MongooseModule.forFeature([
       { name: Candidate.name, schema: CandidateSchema },
       { name: EmployeeProfile.name, schema: EmployeeProfileSchema },
