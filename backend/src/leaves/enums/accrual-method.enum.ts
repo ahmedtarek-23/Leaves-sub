f7059906c08b59d52ed62fbe015886dcd7fe2633
IbrahimMoataz <Hemamoataz@gmail.com>
2025-12-13 19:10:36 +0200

    s

--- backend/src/leaves/enums/accrual-method.enum.ts
+++ backend/src/leaves/enums/accrual-method.enum.ts
@@ -1,21 +1,6 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 export enum AccrualMethod {
   MONTHLY = 'monthly',
   YEARLY = 'yearly',
   PER_TERM = 'per-term',
-  QUARTERLY = 'QUARTERLY', // ADD THIS
-
-=======
-export enum AccrualMethod {
-  MONTHLY = 'monthly',
-  YEARLY = 'yearly',
-  PER_TERM = 'per-term',
->>>>>>> Stashed changes
-=======
-export enum AccrualMethod {
-  MONTHLY = 'monthly',
-  YEARLY = 'yearly',
-  PER_TERM = 'per-term',
->>>>>>> Stashed changes
+  QUARTERLY = 'quarterly',
 }
