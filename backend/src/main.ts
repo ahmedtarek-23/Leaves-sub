e222a0bae3e27f353a329eb60874076bcc7ec019
Rana Abouraia <16abourr@gmail.com>
2025-12-14 18:10:44 +0200

    connecting db

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,14 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { config } from 'dotenv';
+config();   // ← add this line
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Serve static files from uploads directory
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-    prefix: '/uploads/',
-  });
+  // // Serve static files from uploads directory
+  // app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+  //   prefix: '/uploads/',
+  // });
 
   // ★ allow frontend origin + credentials (cookies)
   app.enableCors({
