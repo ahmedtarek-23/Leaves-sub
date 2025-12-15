0b317a966b17354b30a6735c489dff0b382ae75d
ahmedtarek-23 <ionetwo242@gmail.com>
2025-12-15 19:49:53 +0200

    final backend

--- backend/src/leaves/enums/accrual-method.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum AccrualMethod {
-  MONTHLY = 'monthly',
-  YEARLY = 'yearly',
-  PER_TERM = 'per-term',
-  QUARTERLY = 'QUARTERLY', // ADD THIS
-}
--- backend/src/leaves/enums/leave-status.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum LeaveStatus {
-  PENDING = 'pending',
-  APPROVED = 'approved',
-  REJECTED = 'rejected',
-  CANCELLED = 'cancelled',
-  ESCALATED = 'ESCALATED',
-  ENCASHED = 'ENCASHED',
-}
